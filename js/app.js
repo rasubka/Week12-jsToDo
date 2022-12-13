394328f2a76cacd0467f9b39b0fc34936f5975a8
Ranel Subka <rasubka@tktk.ee>
2022-12-13 21:12:11 +0200

    Javascripted ToDo List

---
+++ js/app.js
@@ -0,0 +1,39 @@
+const addToDoButton = document.querySelector('#addTask');
+const inputField = document.querySelector('#userTaskInput');
+const toDoContainer = document.querySelector('.todos');
+
+inputField.addEventListener('click', selectInput);
+addToDoButton.addEventListener('click', addTask)
+
+function selectInput() {
+  inputField.style['boxShadow'] = '0 6px 2px -2px green';
+}
+
+function addTask() {
+  if(inputField.value.trim().length) {
+    //create a new li element
+    let newToDo = document.createElement('li');
+    //add a selector to the element
+    newToDo.classList.add('li-styling');
+    //add content to the element
+    newToDo.innerText = inputField.value;
+    //add the element to a container
+    toDoContainer.appendChild(newToDo);
+
+    newToDo.addEventListener('click', () => {
+      newToDo.style.color = 'grey';
+      newToDo.style.textDecoration = 'line-through';
+    });
+
+    newToDo.addEventListener('dblclick', () => {
+      toDoContainer.removeChild(newToDo);
+    });
+  }
+
+  else {
+    console.log('empty value');
+    inputField.style['boxShadow'] = '0 6px 2px -2px red';
+  }
+
+  inputField.value = '';
+}
